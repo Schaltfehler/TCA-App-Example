40000514af119eff48d82fc9e891596cc601f1ff
Vogel Frederik <vogel.frederik@linecorp.com>
2020-04-21 19:48:37 +0900

    Initial Commit

---
+++ App-Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App-Example
+//
+//  Created by Frederik Vogel on 2020/04/21.
+//  Copyright © 2020 Frederik Vogel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
