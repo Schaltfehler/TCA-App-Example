1c7acce98574d0b9247c1f81947082be7e297ded
Vogel Frederik <vogel.frederik@linecorp.com>
2021-08-28 20:20:45 +0900

    Upgrade Xcode project, iOS Deploy Target to 14.5 and TCA to 0.25.1

---
+++ App-Example/App_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_ExampleApp.swift
+//  App-Example
+//
+//  Created by Frederik Vogel on 2021/08/26.
+//
+
+import SwiftUI
+
+@main
+struct App_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
